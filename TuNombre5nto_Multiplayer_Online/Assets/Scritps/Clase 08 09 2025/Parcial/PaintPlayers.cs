78d25a8278675b2879fc5963f5ccd3dae51f0ed3
Guvunsu <gavryk_fray_rojas@hotmail.com>
2025-09-16 00:15:37 -0600

    falta probarlo

--- TuNombre5nto_Multiplayer_Online/Assets/Scritps/Clase 08 09 2025/Parcial/PaintPlayers.cs
+++ TuNombre5nto_Multiplayer_Online/Assets/Scritps/Clase 08 09 2025/Parcial/PaintPlayers.cs
@@ -1,19 +1,26 @@
 using UnityEngine;
-
-public class PaintPlayers : MonoBehaviour
+using Unity.Netcode;
+public class PaintPlayers : NetworkBehaviour
 {
     /// <summary>
     /// hacer Utilizando la información que otorga unity sobre ID de usuarios a través de la red, deberás pintar al host de rojo y al cliente de azul.
     /// </summary>
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    Renderer renderObj;
+
     void Start()
     {
-        
+        renderObj = GetComponent<Renderer>();
+        if (IsOwner)
+        {
+            if (IsHost)
+                SetColor(Color.red);
+            else if (IsClient)
+                SetColor(Color.blue);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void SetColor(Color c)
     {
-        
+        renderObj.material.color = c;
     }
 }
